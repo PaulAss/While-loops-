1e67f41f4282fec0e0a1aada3592f7dde0b4c80b
Goodness <paul_assiaw@hotmail.co.uk>
2023-01-16 15:36:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int i =1;
+    while (i<6){
+      System.out.println("Hi, how are you doing?");
+      i=i+1;
+    }
+         
+  }
+}
